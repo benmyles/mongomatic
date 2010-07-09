e2063e6e3a85500415f2314b84431564bf99715e
Ben Myles <ben.myles+github@gmail.com>
2010-07-08 17:27:29 -0700

    wrapped more mongo driver cursor methods so that results are what we expect

--- lib/mongomatic/cursor.rb
+++ lib/mongomatic/cursor.rb
@@ -7,7 +7,7 @@ module Mongomatic
       @mongo_cursor = mongo_cursor
       
       @mongo_cursor.public_methods(false).each do |meth|
-        next if ["next_document","each","to_a"].include?(meth.to_s)
+        next if self.methods.include?(meth.to_sym)
         (class << self; self; end).class_eval do
           define_method meth do |*args|
             @mongo_cursor.send meth, *args
@@ -32,6 +32,26 @@ module Mongomatic
     
     def to_a
       @mongo_cursor.to_a.collect { |doc| @obj_class.new(doc) }
+    end
+    
+    def current_limit
+      @mongo_cursor.limit
+    end
+    
+    def limit(number_to_return)
+      @mongo_cursor.limit(number_to_return); self
+    end
+    
+    def current_skip
+      @mongo_cursor.skip
+    end
+    
+    def skip(number_to_skip)
+      @mongo_cursor.skip(number_to_skip); self
+    end
+    
+    def sort(key_or_list, direction = nil)
+      @mongo_cursor.sort(key_or_list, direction); self
     end
   end
 end
--- test/test_meta_mongo.rb
+++ test/test_meta_mongo.rb
@@ -39,4 +39,30 @@ class TestMongomatic < Test::Unit::TestCase
     found  = cursor.next
     assert_nil found
   end
+  
+  should "be able to limit and sort" do
+    Person.collection.remove
+    p = Person.new(:name => "Ben", :birth_year => 1984, :created_at => Time.now.utc, :admin => true)
+    assert p.insert.is_a?(BSON::ObjectID)
+    assert_equal 1, Person.collection.count
+    p2 = Person.new(:name => "Ben2", :birth_year => 1984, :created_at => Time.now.utc, :admin => true)
+    assert p2.insert.is_a?(BSON::ObjectID)
+    assert_equal 2, Person.collection.count
+    
+    cursor = Person.find({"_id" => p["_id"]})
+    found  = cursor.next
+    assert_equal p, found
+    
+    cursor = Person.find()
+    assert_equal 0, cursor.current_limit
+    assert_equal 2, cursor.to_a.size
+    cursor = Person.find().limit(1)
+    assert_equal 1, cursor.current_limit
+    assert_equal 1, cursor.to_a.size
+    cursor = Person.find().limit(1)
+    assert_equal p, cursor.next
+    assert_equal nil, cursor.next
+    cursor = Person.find().limit(1).skip(1)
+    assert_equal p2, cursor.next
+  end
 end
