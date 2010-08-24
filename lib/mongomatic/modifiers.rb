e2cf45a74cff76833ea31e249d14717c0234dd43
Ben Myles <ben@metalevelgames.com>
2010-08-23 17:17:58 -0700

    rewrote push and pull

--- lib/mongomatic/modifiers.rb
+++ lib/mongomatic/modifiers.rb
@@ -2,15 +2,32 @@ module Mongomatic
   # Provides convenience methods for atomic MongoDB operations.
   module Modifiers
 
+    class UnexpectedFieldType < RuntimeError; end
+    
     # MongoDB equivalent: { $push : { field : value } }<br/>
     # Appends value to field, if field is an existing array, otherwise sets field to the array [value] 
-    # if field is not present. If field is present but is not an array, an error condition is raised.
-    #  user.push("interests", "skydiving")
-    def push(field, val, do_reload=true)
+    # if field is not present. If field is present but is not an array, error is returned.
+    def push(field, val, safe=false)
       field = field.to_s
-      if update({}, { "$push" => { field => val } })
-        reload if do_reload; true
+
+      unless self[field].blank? || self[field].is_a?(Array)
+        raise(UnexpectedFieldType)
       end
+      
+      op  = { "$push" => { field => val } }
+      res = true
+      
+      safe == true ? res = update!({}, op) : update({}, op)
+      
+      if res
+        self[field] ||= []
+        self[field] << val
+        true
+      end
+    end
+    
+    def push!(field, val)
+      push(field, val, true)
     end
     
     # MongoDB equivalent: { $pushAll : { field : value_array } }<br/>
@@ -18,23 +35,55 @@ module Mongomatic
     # the array value_array if field is not present. If field is present but is not an array, an error 
     # condition is raised.
     #  user.push("interests", ["skydiving", "coding"])
-    def push_all(field, val, do_reload=true)
+    def push_all(field, val, safe=false)
       field = field.to_s
-      val = Array(val)
-      if update({}, { "$pushAll" => { field => val } })
-        reload if do_reload; true
+      
+      unless self[field].blank? || self[field].is_a?(Array)
+        raise(UnexpectedFieldType)
       end
+      
+      val = Array(val)
+      op  = { "$pushAll" => { field => val } }
+      res = true
+      
+      safe == true ? res = update!({}, op) : update({}, op)
+      
+      if res
+        self[field] ||= []
+        val.each { |v| self[field] << v }
+        true
+      end
+    end
+    
+    def push_all!(field, val)
+      push_all(field, val, true)
     end
     
     # MongoDB equivalent: { $pull : { field : _value } }<br/>
     # Removes all occurrences of value from field, if field is an array. If field is present but is not 
     # an array, an error condition is raised.
     #  user.pull("interests", "watching paint dry")
-    def pull(field, val, do_reload=true)
+    def pull(field, val, safe=false)
       field = field.to_s
-      if update({}, { "$pull" => { field => val } })
-        reload if do_reload; true
+      
+      unless self[field].blank? || self[field].is_a?(Array)
+        raise(UnexpectedFieldType)
       end
+      
+      op  = { "$pull" => { field => val } }
+      res = true
+      
+      safe == true ? res = update!({}, op) : update({}, op)
+      
+      if res
+        self[field] ||= []
+        self[field].delete(val)
+        true
+      end
+    end
+    
+    def pull!(field, val)
+      pull(field, val, true)
     end
     
     # MongoDB equivalent: { $pullAll : { field : value_array } }<br/>
