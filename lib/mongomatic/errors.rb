2a1956ae6616e22d8082f35e74c95ef0d6d82dc3
Jordan West <jordanrw@gmail.com>
2010-11-15 21:08:45 -0800

    Mongomatic::Errors#count

--- lib/mongomatic/errors.rb
+++ lib/mongomatic/errors.rb
@@ -31,6 +31,14 @@ module Mongomatic
     def empty?
       !(@errors.any? { |k,v| v && !v.empty? })
     end
+
+    def any?
+      !empty?
+    end
+    
+    def count
+      @errors.values.inject(0) { |sum, errors| sum += errors.size }
+    end
     
     def full_messages
       full_messages = []
