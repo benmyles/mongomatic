2147eaa3ffa109cf2a2c5daec827aeccad175260
Jordan West <jordan@Jordan-Wests-MacBook-Pro-2.local>
2010-11-18 11:45:06 -0800

    dont let that NameError fly

--- lib/mongomatic/observable.rb
+++ lib/mongomatic/observable.rb
@@ -8,7 +8,7 @@ module Mongomatic
       self.class.observers.each do |observer|
         @observer_cache ||= {}
         unless observer_klass = @observer_cache[observer]
-          @observer_cache[observer] = observer_klass = Object.const_get(observer)
+          @observer_cache[observer] = observer_klass = Object.const_get(observer) if Module.const_defined?(observer)
         end
         instance = observer_klass.new
         instance.send(meth, self) if instance.respond_to?(meth)
