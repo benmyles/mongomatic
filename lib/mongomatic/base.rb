9fd97e46749d324d284f88bae5ab57b45ead7c59
Ben Myles <ben@metalevelgames.com>
2010-11-19 11:45:56 -0800

    transaction() can now take a hash with :scope

--- lib/mongomatic/base.rb
+++ lib/mongomatic/base.rb
@@ -312,7 +312,11 @@ module Mongomatic
     
     def transaction(key=nil, duration=5, &block)
       raise Mongomatic::Exceptions::DocumentIsNew if new?
-      key ||= [self.class.name, self["_id"].to_s].join("-")
+      if key.is_a?(Hash) && key[:scope]
+        key = [self.class.name, self["_id"].to_s, key[:scope]].join("-")
+      else
+        key ||= [self.class.name, self["_id"].to_s].join("-")
+      end
       TransactionLock.start(key, duration, &block)
     end
   end
